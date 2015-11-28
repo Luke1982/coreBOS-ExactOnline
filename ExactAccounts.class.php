0b7d59f67207176881b9b4824cb9e709192c88b8
Guido Goluke <gcx3@hotmail.com>
2015-11-28 18:42:46 +0100

    Creating the ExactAccounts class and listAll function

--- ExactAccounts.class.php
+++ ExactAccounts.class.php
@@ -1,5 +1,12 @@
 <?php
 require('modules/ExactOnline/ExactApi.class.php');
 
+class ExactAccounts extends ExactApi{
+	
+	public function listAll($division, $selection) {
+		$this->sendGetRequest('crm/Accounts', $division, $selection);
+	}
+	
+}
 
 ?>
