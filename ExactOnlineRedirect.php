838ab9f8b782ff0a6c7cda58d54cf01d762c8efa
Guido Goluke <gcx3@hotmail.com>
2015-11-28 18:42:06 +0100

    Obsolete file

--- ExactOnlineRedirect.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This file has only one purpose: to redirect back to the handling file
-// With the GET response code
-header('index.php?module=ExactOnline&action=ExactOnlineAjax&file=test&code='.$_GET['code']);
-?>
