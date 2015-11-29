b3e004f7290b7444620b83eabdd20efbe7fa8045
Guido Goluke <gcx3@hotmail.com>
2015-11-29 21:23:15 +0100

    Setting up the SalesInvoice class

---
+++ ExactSalesInvoice.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class ExactSalesInvoice extends ExactApi{
+	
+	public function __construct() {
+		require_once('vtlib/Vtiger/Module.php');
+		require_once('modules/ExactOnline/functions.php');
+	}
+	
+	public function CreateSalesInvoice() {
+		
+	}
+}
+
+?>
