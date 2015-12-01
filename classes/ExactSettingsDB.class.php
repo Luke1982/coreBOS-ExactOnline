db76f835e417eb3cedd4c31e6d0690742bd360a5
Guido Goluke <gcx3@hotmail.com>
2015-12-01 20:49:19 +0100

    New database for settings, so updated class also

--- classes/ExactSettingsDB.class.php
+++ classes/ExactSettingsDB.class.php
@@ -12,7 +12,7 @@ class ExactSettingsDB {
 	//function to get the value of a specific column from the database
 	public function getDbValue($columnname) {
 		global $adb;
-		$ColumnResult = $adb->pquery('SELECT * FROM vtiger_exactonline WHERE exactonlineid=?',array(0));
+		$ColumnResult = $adb->pquery('SELECT * FROM vtiger_exactonline_settings WHERE exactonlineid=?',array(0));
 		$Field = $adb->query_result($ColumnResult,0,$columnname);
 		return $Field;
 	}
@@ -20,19 +20,19 @@ class ExactSettingsDB {
 	//function to save the access token into the database
 	public function saveAccessToken($value) {
 		global $adb;
-		$adb->pquery('UPDATE vtiger_exactonline SET access_token=? WHERE exactonlineid=0',array($value));
+		$adb->pquery('UPDATE vtiger_exactonline_settings SET access_token=? WHERE exactonlineid=0',array($value));
 	}
 	
 	//function to save the refresh token into the database
 	public function saveRefreshToken($value) {
 		global $adb;
-		$adb->pquery('UPDATE vtiger_exactonline SET refresh_token=? WHERE exactonlineid=0',array($value));
+		$adb->pquery('UPDATE vtiger_exactonline_settings SET refresh_token=? WHERE exactonlineid=0',array($value));
 	}
 	
 	//function to save the refreshed time into the database
 	public function saveRefreshedTime() {
 		global $adb;
-		$adb->pquery('UPDATE vtiger_exactonline SET exactrefreshedtime=? WHERE exactonlineid=0',array(time()));
+		$adb->pquery('UPDATE vtiger_exactonline_settings SET exactrefreshedtime=? WHERE exactonlineid=0',array(time()));
 	}
 	
 }
