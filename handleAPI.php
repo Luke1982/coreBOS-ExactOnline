64408916e42ff9002c1e916a3ebfdc77eaf22c4e
Guido Goluke <gcx3@hotmail.com>
2015-12-02 23:05:10 +0100

    Adds workflow functions to handler API file

--- handleAPI.php
+++ handleAPI.php
@@ -18,7 +18,6 @@
 *************************************************************************************************/
 
 // This file acts as the connector between Exact Online and coreBOS. 
-
 error_reporting(E_ALL);
 ini_set("display_errors", "on");
 
@@ -80,6 +79,139 @@ function sendAccountToExact($entity) {
 		'Status'		=>	'C'
 	);
 	$Account->CreateAccount($division, $accountFields);
+}
+
+function sendProductToExact($entity) {
+	Authenticate();
+	// Include the classes
+	include_once('modules/ExactOnline/classes/includeExactClasses.php');
+	// Get the division
+	$SDB = new ExactSettingsDB();
+	$division = $SDB->getDbValue('exactdivision');
+	$Item = new ExactItems();
+	// Get the GUID for the GLAccount for this product
+	$GLAccountGUID = $Item->getGLAccountGUID($division, $entity->data['product_no']);
+	// Setup the POST array for this product
+	$productPostArray = array(
+		'GLCosts'			=>	$GLAccountGUID,
+		'Code'				=>	$entity->data['product_no'],
+		'Description'		=>	$entity->data['productname']
+	);
+	$Item->sendItem($division, $productPostArray);
+}
+
+function sendServiceToExact($entity) {
+	Authenticate();
+	// Include the classes
+	include_once('modules/ExactOnline/classes/includeExactClasses.php');
+	// Get the division
+	$SDB = new ExactSettingsDB();
+	$division = $SDB->getDbValue('exactdivision');
+	$Item = new ExactItems();
+	// Get the GUID for the GLAccount for this product, we need to specify an extract
+	// parameter here to tell the method to look in the services table
+	$GLAccountGUID = $Item->getGLAccountGUID($division, $entity->data['service_no'], TRUE);
+	// Setup the POST array for this product
+	$servicePostArray = array(
+		'GLCosts'			=>	$GLAccountGUID,
+		'Code'				=>	$entity->data['service_no'],
+		'Description'		=>	$entity->data['servicename']
+	);
+	$Item->sendItem($division, $servicePostArray);
+}
+
+function syncGLAccounts() {
+	// This function actually empties the vtiger_generalledgers
+	// table completely and uses the 'getGLAccounts' method of
+	// 'ExactItems' class to re-fill it. Individual products will
+	// need to be updated after this
+	Authenticate();
+	// Include the classes
+	include_once('modules/ExactOnline/classes/includeExactClasses.php');
+	// Get the division
+	$SDB = new ExactSettingsDB();
+	$division = $SDB->getDbValue('exactdivision');
+	$Item = new ExactItems();
+	$Item->updateGLAccounts($division);
+}
+
+function sendInvoiceToExact($entity) {
+	Authenticate();
+	// Include the classes
+	include_once('modules/ExactOnline/classes/includeExactClasses.php');
+	// Get the division
+	$SDB = new ExactSettingsDB();
+	$division = $SDB->getDbValue('exactdivision');
+	// Instantiate the sales invoice class and execute creation
+	$SI = new ExactSalesInvoice();
+	$SI->CreateSalesInvoice($division, $entity->data['invoice_no']);
+}
+
+function createGLAccountField() {
+	// Authenticate at Exact first
+	Authenticate();
+	// Include the classes
+	include_once('modules/ExactOnline/classes/includeExactClasses.php');
+	global $adb;
+	// Get the division
+	$SDB = new ExactSettingsDB();
+	$division = $SDB->getDbValue('exactdivision');
+	$Items = new ExactItems();
+	$GLAccountValues = $Items->getGLAccounts($division);
+	// The actual function for creating the GL Account fields
+	// Beware, this has nothing to do with the Accounts module
+	// It is an accounting term for Product Groups
+	$Vtiger_Utils_Log = true;
+	include_once('vtlib/Vtiger/Menu.php');
+	include_once('vtlib/Vtiger/Module.php');
+	$module = Vtiger_Module::getInstance('Products');
+	$infoBlock = Vtiger_Block::getInstance('LBL_PRODUCT_INFORMATION', $module);
+	$glaccountsField = Vtiger_Field::getInstance('generalledgers', $module);
+	if (!$glaccountsField) {
+		$glaccountsField = new Vtiger_Field();
+		$glaccountsField->name = 'generalledgers';
+		$glaccountsField->label = 'General Ledger Accounts';
+		$glaccountsField->column = 'generalledgers';
+		$glaccountsField->columntype = 'VARCHAR(255)';
+		$glaccountsField->uitype = 16;
+		$glaccountsField->typeofdata = 'V~M';
+		$glaccountsField->setPicklistValues($GLAccountValues);
+		$infoBlock->addField($glaccountsField);
+	}
+}
+
+function createGLAccountServiceField() {
+	// Authenticate at Exact first
+	Authenticate();
+	// Include the classes
+	include_once('modules/ExactOnline/classes/includeExactClasses.php');
+	global $adb;
+	// Get the division
+	$SDB = new ExactSettingsDB();
+	// $division = $SDB->getDbValue('exactdivision');
+	$Items = new ExactItems();
+	// Get all the General Ledgers in an array
+	$GLAccountValues = $Items->getGLAccounts($division);
+	// The actual function for creating the GL Account fields
+	// Beware, this has nothing to do with the Accounts module
+	// It is an accounting term for Product Groups
+	$Vtiger_Utils_Log = true;
+	include_once('vtlib/Vtiger/Menu.php');
+	include_once('vtlib/Vtiger/Module.php');
+	$module = Vtiger_Module::getInstance('Services');
+	$infoBlock = Vtiger_Block::getInstance('LBL_SERVICE_INFORMATION', $module);
+	$glaccountsField = Vtiger_Field::getInstance('glservices', $module);
+	if (!$glaccountsField) {
+		$glaccountsField = new Vtiger_Field();
+		$glaccountsField->name = 'glservices';
+		$glaccountsField->label = 'General Ledger Accounts';
+		$glaccountsField->column = 'glservices';
+		$glaccountsField->columntype = 'VARCHAR(255)';
+		$glaccountsField->uitype = 16;
+		$glaccountsField->typeofdata = 'V~M';
+		$glaccountsField->setPicklistValues($GLAccountValues);
+		$infoBlock->addField($glaccountsField);
+	}
 }
 
 ?>
