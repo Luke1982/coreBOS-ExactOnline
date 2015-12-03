cff168ad0f70ef69e71310da8dd7edc02738aa1f
Guido Goluke <gcx3@hotmail.com>
2015-12-03 15:23:32 +0100

    Start the JS file, function to save credentials

---
+++ modules/ExactOnline/ExactOnline.js
@@ -0,0 +1,38 @@
+/*************************************************************************************************
+ * Copyright 2015 MajorLabel -- This file is a part of MajorLabel coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. MajorLabel reserves all rights not expressly
+ * granted by the License. coreBOS distributed by MajorLabel is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+*************************************************************************************************
+*  Author       : MajorLabel, Guido Goluke
+*************************************************************************************************/
+
+jQuery(window).load(function(){
+	
+	var saveCreds = function() {
+		jQuery.ajax({
+			type: "POST",
+			data:	{
+				'division'		:	jQuery('input#division').val(),
+				'clientID'		:	jQuery('input#clientID').val(),
+				'clientsecret'	:	jQuery('input#clientsecret').val(),
+				'save'			:	true
+			},
+			success: alert('Settings Saved')
+		});
+	}
+	
+	jQuery('#setCredentialsForm').submit(function(event){
+		event.preventDefault();
+		saveCreds();
+	});
+	
+});
