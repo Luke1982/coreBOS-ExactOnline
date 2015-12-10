2cafc40b858adce87c1ce706872fc01b27d27290
Guido Goluke <gcx3@hotmail.com>
2015-12-10 14:57:50 +0100

    Remove duedate and automate type
    You can't POST of PUT duedate, only GET, so this is removed. Also, the
    invoice type will now be set to the Exact-desired '8021' in case of a
    credit invoice and '8020' otherwise.

--- modules/ExactOnline/classes/ExactSalesInvoice.class.php
+++ modules/ExactOnline/classes/ExactSalesInvoice.class.php
@@ -12,13 +12,19 @@ class ExactSalesInvoice extends ExactApi{
 		global $adb;
 		$Account = new ExactAccounts();
 		// Get some more information from the invoice
-		$IR = $adb->pquery('SELECT subject, salesorderid, duedate FROM vtiger_invoice WHERE invoice_no=?', array($invoiceno));
+		$IR = $adb->pquery('SELECT subject, salesorderid, status FROM vtiger_invoice WHERE invoice_no=?', array($invoiceno));
 		$invoiceData = $adb->query_result_rowdata($IR,0);
 		// Get the salesorder from this invoice from the database
 		$invoiceRelSalesOrder = $adb->pquery('SELECT createdtime FROM vtiger_crmentity WHERE crmid=?',array($invoiceData['salesorderid']));
 		$invoiceRelSalesOrderDate = $adb->query_result($invoiceRelSalesOrder,0,'createdtime');
 		// Cut off the time by a simple substr function
 		$invoiceRelSalesOrderDate = substr($invoiceRelSalesOrderDate, 0, 10);
+		// Set the type: always 8020, except when status is 'Credit Invoice', then 8021
+		if ( $invoiceData['status'] == 'Credit Invoice' ) {
+			$invoiceType = 8021;
+		} else {
+			$invoiceType = 8020;
+		}
 		// Get the Account number related to this invoice
 		$accountResultforInvoice = $adb->pquery('SELECT account_no FROM vtiger_account LEFT JOIN vtiger_invoice ON vtiger_account.accountid=vtiger_invoice.accountid WHERE vtiger_invoice.invoice_no=?',array($invoiceno));
 		$AccNoForThisInvoice = $adb->query_result($accountResultforInvoice,0,'account_no');
@@ -35,9 +41,8 @@ class ExactSalesInvoice extends ExactApi{
 			'Journal'			=>	'50',
 			'OrderedBy'			=>	$AccGuidForThisInv,
 			'InvoiceTo'			=>	$AccGuidForThisInv,
-			'DueDate'			=>	$invoiceData['duedate'],
 			'OrderDate'			=>	$invoiceRelSalesOrderDate,
-			'Type'				=>	8020,
+			'Type'				=>	$invoiceType,
 			'OrderNumber'		=>	$invoiceno,
 			'Description'		=>	$invoiceData['subject'],
 			'SalesInvoiceLines'	=>	$InvoiceLines
